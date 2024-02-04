69714abb424396362e94950992706b3327098a51
EC2 Default User <ec2-user@ip-172-31-21-43.ap-northeast-1.compute.internal>
2024-02-04 04:29:12 +0000

    cutomersのログイン

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -2,13 +2,17 @@
 
 class Public::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
+  
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number])
   end
   
-  def create
-    
-  end
+  
+  
+  
+  
+  
+  
   
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
+  
   scope module: :public do
     root to: 'homes#top'
+    get '/customers' => 'customers#show'
+    get '/customers/information/:id/edit' => 'customers#edit'
+    patch '/customers/information/:id' => 'customers#update'
+    get '/customers/confirm_withdraw' => 'customers#confirm_withdraw'
+    patch '/customers/withdraw' => 'customers#withdraw'
+    
+    
     get '/about' => 'homes#about'
+    get '/items' => 'items#index'
+    get '/items/:id' => 'items#show'
     
   end
   
