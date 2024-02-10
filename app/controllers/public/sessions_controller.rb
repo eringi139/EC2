79fbc60eface8aa7b73c1906b59fae68653873e0
EC2 Default User <ec2-user@ip-172-31-21-43.ap-northeast-1.compute.internal>
2024-02-10 03:13:34 +0000

    admin商品新規登録

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   def after_sign_in_path_for(resource)
-  customer_path(current_customer.id)
+    root_path(current_customer.id)
   end
   
  def after_sign_out_path_for(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  
   devise_for :customers, skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
@@ -19,6 +20,8 @@ Rails.application.routes.draw do
   
   namespace :admin do
     root to: 'homes#top'
+    
+    resources :items, only: [:index, :new, :create, :show, :edit, :update]
   end
   
 devise_for :admin, skip: [:registrations, :passwords], controllers: {
