a7219d4e1e4691c7b2cdf097b4c386d7c694ce7e
EC2 Default User <ec2-user@ip-172-31-21-43.ap-northeast-1.compute.internal>
2024-02-11 05:16:53 +0000

    admin商品新規登録

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+}
   
   devise_for :customers, skip: [:passwords], controllers: {
   registrations: "public/registrations",
@@ -18,15 +21,15 @@ Rails.application.routes.draw do
     
   end
   
+  
+  
   namespace :admin do
     root to: 'homes#top'
     
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
   end
   
-devise_for :admin, skip: [:registrations, :passwords], controllers: {
-  sessions: "admin/sessions"
-}
+
 
 
 
