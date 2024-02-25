d090c451dc1311ef45638a8db0aea34d713b4b00
EC2 Default User <ec2-user@ip-172-31-21-43.ap-northeast-1.compute.internal>
2024-02-25 04:42:05 +0000

    注文確定

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -5,9 +5,20 @@ class Public::OrdersController < ApplicationController
   end 
 
   def confirm
+   @order = Order.new(order_params)
    @cart_items = CartItem.where(customer_id: current_customer.id)
-   @shipping_fee = 800
-   @selected_pay_method = params[:order][:pey_method]
+  
+   if params[:order][:address_method] == "0"
+        @order.post_code = current_customer.postal_code
+        @order.address = current_customer.address
+        @order.name = current_customer.last_name + current_customer.first_name 
+    elsif params[:order][:address_method] = "2"
+        @order.post_code = params[:order][:postal_code]
+        @order.address = params[:order][:address]
+        @order.name = params[:order][:name]
+    else
+    end
+    @cart_item = current_customer.cart_item.all
   end
 
   def thanx
