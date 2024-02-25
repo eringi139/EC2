50fd04d8011a68be281637ad1f2e72b3687d77d4
EC2 Default User <ec2-user@ip-172-31-21-43.ap-northeast-1.compute.internal>
2024-02-25 11:55:28 +0000

    注文情報確定画面

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,30 +1,32 @@
 class Public::OrdersController < ApplicationController
- 
+
   def new
     @order = Order.new
-  end 
+  end
 
   def confirm
    @order = Order.new(order_params)
    @cart_items = CartItem.where(customer_id: current_customer.id)
-  
+
    if params[:order][:address_method] == "0"
-        @order.post_code = current_customer.postal_code
+        @order.postal_code = current_customer.postal_code
         @order.address = current_customer.address
-        @order.name = current_customer.last_name + current_customer.first_name 
+        @order.name = current_customer.last_name + current_customer.first_name
     elsif params[:order][:address_method] = "2"
         @order.post_code = params[:order][:postal_code]
         @order.address = params[:order][:address]
         @order.name = params[:order][:name]
     else
     end
-    @cart_item = current_customer.cart_item.all
+    @cart_item = current_customer.cart_items.all
   end
 
   def thanx
   end
 
   def create
+     @order = Order.new(order_params)
+     @order.save
   end
 
   def index
@@ -32,9 +34,9 @@ class Public::OrdersController < ApplicationController
 
   def show
   end
-  
+
   private
 def order_params
-  params.require(:order).permit(:pay_method, :postal_code, :address, :name)
+  params.require(:order).permit(:pay_method, :customer_id, :postal_code, :address, :name)
 end
 end
