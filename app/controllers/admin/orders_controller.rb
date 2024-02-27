8a377898f31d5e471830622b368d70058fa5e7a2
EC2 Default User <ec2-user@ip-172-31-21-43.ap-northeast-1.compute.internal>
2024-02-27 14:08:20 +0000

    注文詳細

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,7 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = Order.find(params[:id])
+    @orders = Order.all
+    @order_details = OrderDetail.where(order_id: @order)
   end
 end
